d796068967d563167c98bc2e54e704241628dd5b
Wesley-waka <wesleywaka77@gmail.com>
2023-12-27 16:26:28 +0300

    DB Schema:reports

--- db/migrate/20231227131120_create_reports.rb
+++ db/migrate/20231227131120_create_reports.rb
@@ -1,7 +1,14 @@
 class CreateReports < ActiveRecord::Migration[7.0]
   def change
     create_table :reports do |t|
-      t.string :name
+      t.string :description
+      t.string :image
+      t.string :video
+      t.string :gps_coordinates
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :report_status, null: false, foreign_key: true
+      t.belongs_to :report_type, null: false, foreign_key: true
+
       t.timestamps
     end
   end
--- db/migrate/20231227131256_create_report_types.rb
+++ db/migrate/20231227131256_create_report_types.rb
@@ -1,6 +1,7 @@
 class CreateReportTypes < ActiveRecord::Migration[7.0]
   def change
     create_table :report_types do |t|
+      t.string :name
 
       t.timestamps
     end
